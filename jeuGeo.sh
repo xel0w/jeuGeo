6a85d497ab2e9e159357461a61a093b7dd553bfe
Xel0w <jordan.byrgiel@hotmail.fr>
2023-05-10 13:09:42 +0200

    4 commit

--- jeuGeo.sh
+++ jeuGeo.sh
@@ -18,91 +18,21 @@ function est_code_postal_valide() {
   fi
 }
 
-# Fonction pour vérifier si le cache est valide
-function cache_est_valide() {
-  local cache_file=$1
-  local cache_duration=$2
-  local refresh_cache=$3
-  local cache_validity=0
-
-  if [ -f "$cache_file" ]; then
-    local cache_timestamp=$(stat -c %Y "$cache_file")
-    local current_timestamp=$(date +%s)
-    local time_diff=$((current_timestamp - cache_timestamp))
-    
-    if [ $time_diff -le $cache_duration ]; then
-      cache_validity=1
-    fi
-  fi
-  
-  if [ $cache_validity -eq 0 ] || [ "$refresh_cache" = "--refreshcache" ]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
 # Fonction pour interroger l'API et récupérer les informations sur les communes
 function recuperer_informations_communes() {
   local code_postal=$1
-  local cache_file=".cache_$code_postal.txt"
-  local cache_duration=$((60 * 60)) # Durée de vie du cache : 60 minutes
-  
-  if cache_est_valide "$cache_file" $cache_duration $2; then
-    # Utilisation du cache
-    communes_array=($(cat "$cache_file"))
-  else
-    # Requête à l'API
-    communes_array=($(curl -s "https://geo.api.gouv.fr/communes?codePostal=$code_postal" | jq -r '.[].nom'))
+  local communes_array=()
 
-    # Mise à jour du cache
-    echo "${communes_array[@]}" > "$cache_file"
-  fi
-}
+  # Requête à l'API
+  communes_json=$(curl -s "https://geo.api.gouv.fr/communes?codePostal=$code_postal")
 
-# Fonction pour jouer au jeu
-function jouer_jeu_geo() {
-  local communes_array=("$@")
-  local nb_vies=10
-  local commune_index=$((RANDOM % ${#communes_array[@]}))
-  local nb_habitants
-  local user_input
-  
-  echo "Avec quelle ville souhaitez-vous jouer ?"
-  afficher_menu communes_array
-  
-  while true; do
-    read -p "Choisissez un numéro de ville : " user_input
-    if [[ $user_input =~ ^[0-9]+$ ]] && [ $user_input -ge 0 ] && [ $user_input -lt ${#communes_array[@]} ]; then
-      break
-    else
-      echo "Numéro invalide. Veuillez choisir un numéro valide."
-    fi
-  done
-  
-  echo "Bienvenue dans le jeu GEO !"
-  echo "Devinez le nombre d'habitants dans la ville ${communes_array[$user_input]}."
-  
-  while [ $nb_vies -gt 0 ]; do
-    read -    p "Nombre d'habitants : " nb_habitants
+  # Extraction des noms des communes
+  while IFS= read -r line; do
+    commune=$(echo "$line" | jq -r '.nom')
+    communes_array+=("$commune")
+  done <<< "$communes_json"
 
-    if [[ $nb_habitants =~ ^[0-9]+$ ]]; then
-      if [ $nb_habitants -eq $nb_habitants_estime ]; then
-        echo "Bravo ! Vous avez trouvé le nombre d'habitants dans la ville ${communes_array[$user_input]}."
-        return 0
-      elif [ $nb_habitants -lt $nb_habitants_estime ]; then
-        echo "Plus grand !"
-      else
-        echo "Plus petit !"
-      fi
-      nb_vies=$((nb_vies - 1))
-      echo "Il vous reste $nb_vies vies."
-    else
-      echo "Saisie invalide. Veuillez saisir un nombre entier."
-    fi
-  done
-
-  echo "Désolé, vous avez perdu ! Le nombre d'habitants dans la ville ${communes_array[$user_input]} était $nb_habitants_estime."
+  echo "${communes_array[@]}"
 }
 
 # Vérification de la présence d'un argument
@@ -111,7 +41,7 @@ if [ $# -eq 1 ]; then
 else
   while true; do
     read -p "Veuillez saisir un code postal : " code_postal
-    if est_code_postal_valide $code_postal; then
+    if est_code_postal_valide "$code_postal"; then
       break
     else
       echo "Code postal invalide. Veuillez saisir un code postal valide."
@@ -120,8 +50,57 @@ else
 fi
 
 # Récupération des informations sur les communes correspondantes au code postal
-recuperer_informations_communes $code_postal $2
+communes_array=($(recuperer_informations_communes "$code_postal"))
+
+# Vérification s'il y a des communes disponibles
+if [ ${#communes_array[@]} -eq 0 ]; then
+  echo "Aucune commune trouvée pour ce code postal."
+  exit 1
+fi
+
+# Affichage du menu des communes
+echo "Avec quelle ville souhaitez-vous jouer ?"
+afficher_menu communes_array
+
+# Définition du nombre de vies
+nb_vies=10
+
+# Sélection de la commune
+while true; do
+  read -p "Choisissez un numéro de ville : " choix_ville
+  if [[ $choix_ville =~ ^[0-9]+$ ]] && [ $choix_ville -ge 0 ] && [ $choix_ville -lt ${#communes_array[@]} ]; then
+    break
+  else
+    echo "Numéro invalide. Veuillez choisir un numéro valide."
+  fi
+done
+
+ville_selectionnee="${communes_array[$choix_ville]}"
+echo "Bienvenue dans le jeu GEO ! Vous avez choisi la ville $ville_selectionnee."
 
 # Jeu GEO
-jouer_jeu_geo "${communes_array[@]}"
+while [ $nb_vies -gt 0 ]; do
+  read -p "Devinez le nombre d'habitants dans la ville $ville_selectionnee : " estimation
 
+  if [[ $estimation =~ ^[0-9]+$ ]]; then
+    habitants=$(curl -s "https://geo.api.gouv.fr/communes?nom=$ville_selectionnee&codePostal=$code_postal" | jq -r '.[0]')
+  if [ $estimation -eq $habitants ]; then
+    echo "Félicitations ! Vous avez deviné le bon nombre d'habitants."
+    break
+  elif [ $estimation -lt $habitants ]; then
+    echo "C'est moins. Essayez encore !"
+  else
+    echo "C'est plus. Essayez encore !"
+  fi
+
+  nb_vies=$((nb_vies - 1))
+
+  if [ $nb_vies -eq 0 ]; then
+    echo "Dommage ! Vous avez épuisé toutes vos vies."
+    break
+  fi
+
+  echo "Il vous reste $nb_vies vies."
+done
+
+echo "Merci d'avoir joué au jeu GEO !"
